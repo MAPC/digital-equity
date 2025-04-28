a9c0a8930ca089514197e3d7a8272016c22da24e
zoeiacovino <zoe.m.iacovino@gmail.com>
2025-04-28 13:07:21 -0400

    4/25

--- chas_setup.R
+++ chas_setup.R
@@ -22,3 +22,64 @@ hud_chas_request <- GET("https://www.huduser.gov/hudapi/public/chas?type=3&year=
 
 # Succesful output: [1] 200
 hud_chas_request$status_code
+
+
+# Accessing Data
+content(hud_chas_request, as = "text")
+
+content_test<-hud_chas_request %>%
+  content(as = "text") %>%
+  fromJSON()
+
+# Writing an Access Function
+get_hud_chas <- function(
+    type, 
+    year = "2016-2020", 
+    state_id = NULL, 
+    entity_id = NULL, 
+    token = NULL
+) {
+  
+  # Check to see if a token exists
+  if (is.null(token)) {
+    if (Sys.getenv("HUD_TOKEN") != "") {
+      token <- Sys.getenv("HUD_TOKEN")
+    }
+  }
+  
+  # Specify the base URL
+  base_url <- "https://www.huduser.gov/hudapi/public/chas"
+  
+  # Make the query
+  hud_query <- httr::GET(
+    base_url,
+    httr::add_headers(Authorization = glue::glue("Bearer {token}")),
+    query = list(
+      type = type,
+      year = year,
+      stateId = state_id,
+      entityId = entity_id)
+  )
+  
+  # Return the HTTP error message if query failed
+  if (hud_query$status_code != "200") {
+    msg <- httr::content(hud_query, as = "text")
+    return(msg)
+  }
+  
+  # Return the content as text
+  hud_content <- httr::content(hud_query, as = "text")
+  
+  # Convert the data to a long-form tibble
+  hud_tibble <- hud_content %>%
+    jsonlite::fromJSON() %>%
+    dplyr::as_data_frame() %>%
+    tidyr::pivot_longer(cols = !dplyr::all_of(c("geoname", "sumlevel", "year")),
+                        names_to = "indicator",
+                        values_to = "value")
+
+  return(hud_tibble)
+  
+}
+
+chazzed<-get_hud_chas(type = 4, state_id = 25, entity_id = 617)
